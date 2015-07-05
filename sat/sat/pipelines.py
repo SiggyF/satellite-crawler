3e94ecc24368bb0ac06e501db1c9c81bec4e5344
Fedor Baart <fedor.baart@deltares.nl>
2015-07-06 00:08:15 +0200

    exception

--- sat/sat/pipelines.py
+++ sat/sat/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.utils.conf import get_config
-
+from scrapy.exceptions import DropItem
 import pika.credentials
 import pika
 import json
